f0ab942e753e5d43d485c4963bd36ffa4beed4f0
unknown <anblackbolt14@gmail.com>
2020-01-12 21:58:18 -0500

    Fixed player movements script

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,27 +5,35 @@ export (int) var speed = 200
 var velocity = Vector2()
 var animPlayer
 var speedMultiplier = 8
+var movement
 
 func _ready():
 	set_physics_process(true)
 	animPlayer = get_node("AnimationPlayer")
 
-func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed("Right"):
-        velocity.x += 1
-				animPlayer.play("Walk Right", -speedMultiplier)
-    if Input.is_action_pressed("Left"):
-        velocity.x -= 1
-				animPlayer.play("Walk Left", -speedMultiplier)
-    if Input.is_action_pressed("Down"):
-        velocity.y += 1
-				animPlayer.play("Walk Down", -speedMultiplier)
-    if Input.is_action_pressed("Up"):
-        velocity.y -= 1
-				animPlayer.play("Walk Up", -speedMultiplier)
-    velocity = velocity.normalized() * speed
+func getInput():
+	velocity = Vector2()
+	if Input.is_action_pressed("Right"):
+		movement = "Walk Right"
+		velocity.x += 1
+		animPlayer.play(movement, -speedMultiplier)
+	elif Input.is_action_pressed("Left"):
+		movement = "Walk Left"
+		velocity.x -= 1
+		animPlayer.play(movement, -speedMultiplier)
+	elif Input.is_action_pressed("Down"):
+		movement = "Walk Down"
+		velocity.y += 1
+		animPlayer.play(movement, -speedMultiplier)
+	elif Input.is_action_pressed("Up"):
+		movement = "Walk Up"
+		velocity.y -= 1
+		animPlayer.play(movement, -speedMultiplier)
+	else:
+		movement = "Idle"
+		animPlayer.play(movement, -speedMultiplier)
+	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
-    get_input()
-    velocity = move_and_slide(velocity)
+	getInput()
+	velocity = move_and_slide(velocity)
