70c7fd3ac88b47f9c86dced2a4f285628d8a61c3
Kelsey Joseph <dojo565@gmail.com>
2020-01-12 22:48:39 -0500

    Changed diamond png file. Organized animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,27 +5,28 @@ export (int) var speed = 200
 var velocity = Vector2()
 var animPlayer
 var speedMultiplier = 8
+var movement = "Idle"
 
 func _ready():
 	set_physics_process(true)
 	animPlayer = get_node("AnimationPlayer")
 
-func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed("Right"):
-        velocity.x += 1
-				animPlayer.play("Walk Right", -speedMultiplier)
-    if Input.is_action_pressed("Left"):
-        velocity.x -= 1
-				animPlayer.play("Walk Left", -speedMultiplier)
-    if Input.is_action_pressed("Down"):
-        velocity.y += 1
-				animPlayer.play("Walk Down", -speedMultiplier)
-    if Input.is_action_pressed("Up"):
-        velocity.y -= 1
-				animPlayer.play("Walk Up", -speedMultiplier)
-    velocity = velocity.normalized() * speed
+func getInput():
+	velocity = Vector2()
+	if Input.is_action_pressed("Right"):
+		velocity.x += 1
+		animPlayer.play("Walk Right", -speedMultiplier)
+	if Input.is_action_pressed("Left"):
+		velocity.x -= 1
+		animPlayer.play("Walk Left", -speedMultiplier)
+	if Input.is_action_pressed("Down"):
+		velocity.y += 1
+		animPlayer.play("Walk Down", -speedMultiplier)
+	if Input.is_action_pressed("Up"):
+		velocity.y -= 1
+		animPlayer.play("Walk Up", -speedMultiplier)
+	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
-    get_input()
-    velocity = move_and_slide(velocity)
+	getInput()
+	velocity = move_and_slide(velocity)
