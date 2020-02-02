81f49a4479c302a5cbcc1721a7b14691f55d7105
Kelsey Joseph <dojo565@gmail.com>
2020-02-01 22:48:34 -0500

    Added collision to diamond, and player

--- Scripts/Diamond.gd
+++ Scripts/Diamond.gd
@@ -5,25 +5,35 @@ enum {
 	SPARKLE,
 	SHINE
 }
-var state = SHINE
+
+var state = SPARKLE
+var newState = SHINE
+var prevState
 var animPlayer
 var animation = "Pause"
 var speedMultiplier = 8
-
+onready var timer = get_node("Timer")
 
 func _ready():
 	animPlayer = get_node("AnimationPlayer")
-
+	timer.set_wait_time(3)
+	timer.start()
 
 func _process(delta):
+	playAnim()
+
+func playAnim():
 	match state:
-		PAUSE:
-			animation = "Pause"
-			animPlayer.play(animation, -speedMultiplier)
-		SHINE:
-			animation = "Shine"
-			animPlayer.play(animation, -speedMultiplier)
-		SPARKLE:
-			animation = "Sparkle"
-			animPlayer.play(animation, -speedMultiplier)
-			
+			SHINE:
+				animation = "Shine"
+				animPlayer.play(animation, -speedMultiplier)
+			SPARKLE:
+				animation = "Sparkle"
+				animPlayer.play(animation, -speedMultiplier)
+
+func TimerTimeout():
+	prevState = state
+	if prevState == state:
+		state = newState
+		newState = prevState
+
