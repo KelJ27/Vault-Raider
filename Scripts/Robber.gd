987f31a0f1d6b1236e374e6424cb5ba40c23f8a4
unknown <anblackbolt14@gmail.com>
2020-02-05 22:07:58 -0500

    Robber script has been added

--- Scripts/Robber.gd
+++ Scripts/Robber.gd
@@ -1,2 +1,25 @@
 extends KinematicBody2D
 
+var speed = 150
+
+func _ready():
+	set_physics_process(true)
+	
+	
+func _fixed_process(delta):
+	var Move = Vector2()
+	var body = get_node("body").get_overlapping_bodies()
+	if(body.size() != 0):
+		for tinge in body:
+			if(tinge.is_in_group("Player")):
+				if(tinge.get_global_position().x < self.get_global_position().x):
+					Move += Vector2(-1,0)
+				if(tinge.get_global_position().x > self.get_global_position().x + 5):
+					Move += Vector2(1,0)
+				if(tinge.get_global_position().y < self.get_global_position().y +5):
+					Move += Vector2(0,-1)
+				if(tinge.get_global_position().y > self.get_global_position().y):
+					Move += Vector2(0,1)
+					
+	Move = Move.normalized() * speed * delta
+	Move = move_and_slide(Move)
